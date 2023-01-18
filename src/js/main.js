b6892c8285a2b7c44e8ff746bab45ccb4f692338
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-01-18 12:54:41 +0200

    Added task: home work N-01, event on left mouse click and push key Alt

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+// 1: По натисканню кнопки alt ми зменьшуемо єлемент але не меньше його розміру
+// 2: Екстра, робити не більше довжини єкрану
+
+const box = document.querySelector(".box");
+let boxWidth = box.offsetWidth;
+let boxHeight = box.offsetHeight;
+const step = 5;
+
+box.addEventListener("click", function (event) {
+  if (event.altKey) {
+    if (boxWidth < 130) {
+      box.innerHTML = "";
+    }
+    if (boxWidth > 10) {
+      box.style.width = `${(boxWidth = boxWidth - step)}px`;
+    }
+  } else {
+    if (boxWidth > 130) {
+      box.innerHTML = `<div class="boxWidth">boxWidth: ${boxWidth}</div>
+      <div class="clientWidth">clientWidth: ${document.documentElement.clientWidth}</div>`;
+    }
+    if (boxWidth < document.documentElement.clientWidth - step * 2) {
+      box.style.width = `${(boxWidth = boxWidth + 10)}px`;
+    }
+  }
+});
